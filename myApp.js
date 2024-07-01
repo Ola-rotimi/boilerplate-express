0198ce73c6266a7906ea04bdb966b678d4518b8d
Olarotimi Adamson <olarotimiadamson@yahoo.com>
2024-07-01 15:00:32 +0000

    express intro

--- myApp.js
+++ myApp.js
@@ -1,15 +1,22 @@
 let express = require('express');
 let app = express();
+require("dotenv").config()
 
+let absolutePath = __dirname + "/views/index.html";
 
+app.get("/", (req, res) => {
+    res.sendFile(absolutePath);
+})
+app.use("/public", express.static(__dirname + "/public"));
 
-
-
-
-
-
-
-
+app.get("/json", (req, res) => {
+    if (process.env.MESSAGE_STYLE == "uppercase") {
+        response = "Hello json".toUpperCase();
+    } else {
+       response = "Hello json";
+    }
+    res.json({"message": response});
+})
 
 
 
