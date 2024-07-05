426f67f36df9796bb7e8b1d6c9d02f5eb8146f95
Olarotimi Adamson <olarotimiadamson@yahoo.com>
2024-07-05 13:49:14 +0000

    post request

--- myApp.js
+++ myApp.js
@@ -10,40 +10,42 @@ app.get("/", (req, res) => {
 })
 app.use("/public", express.static(__dirname + "/public"));
 
-// app.get("/json", (req, res, next) => {
-//     console.log(`${req.method} ${req.path} - ${req.ip}`);
-//     next();
-// }, (req, res) => {
-//     if (process.env.MESSAGE_STYLE == "uppercase") {
-//         response = "Hello json".toUpperCase();
-//     } else {
-//         response = "Hello json";
-//     }
-//     res.json({ "message": response });
-// });
+app.get("/json", (req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+}, (req, res) => {
+    if (process.env.MESSAGE_STYLE == "uppercase") {
+        response = "Hello json".toUpperCase();
+    } else {
+        response = "Hello json";
+    }
+    res.json({ "message": response });
+});
 
-// app.get("/now", (req, res, next) => {
-//     req.time = new Date().toString();
-//     next();
-// }, (req, res) => {
-//     res.send({ time: req.time });
-// });
+app.get("/now", (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.send({ time: req.time });
+});
 
-// app.get("/:word/echo", (req, res) => {
-//     const { word } = req.params
-//     res.json({ echo: word })
-// })
+app.get("/:word/echo", (req, res) => {
+    const { word } = req.params
+    res.json({ echo: word })
+})
 
-// app.get("/name", (req, res)=> {
-//     const {first: firstname, last: lastname} = req.query;
-//     res.json({name: `${firstname} ${lastname}`})
-// })
+app.get("/name", (req, res)=> {
+    const {first: firstname, last: lastname} = req.query;
+    res.json({name: `${firstname} ${lastname}`})
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(bodyParser.json())
+
 app.post("/name", (req, res) => {
     const { first: firstname, last: lastname } = req.body
-    res.json({ name: `${firstname}, ${lastname}` });
+    res.json({ name: `${firstname} ${lastname}` });
 })
 
 
